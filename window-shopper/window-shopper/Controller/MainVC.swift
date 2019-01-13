51fae0d4e7fdc0522ebf18682a194445d2b2f43e
Cody <Bailey@192-168-1-6.tpgi.com.au>
2019-01-13 19:03:20 +0800

    lesson4

--- window-shopper/window-shopper/Controller/MainVC.swift
+++ window-shopper/window-shopper/Controller/MainVC.swift
@@ -15,9 +15,16 @@ class MainVC: UIViewController {
     @IBOutlet weak var priceText: CurrencyTextField!
     
     
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var hoursLbl: UILabel!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
         
         let calcBtn = UIButton(frame:  CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
         calcBtn.backgroundColor = #colorLiteral(red: 1, green: 0.6484236862, blue: 0.2012576967, alpha: 1)
@@ -30,8 +37,28 @@ class MainVC: UIViewController {
     }
     
     @objc func calculate() {
+        if let wageTxt = wageText.text, let priceTxt = priceText.text {
+            if let wage = Double(wageTxt), let price = Double(priceTxt) {
+                view.endEditing(true)
+                resultLbl.isHidden = false
+                hoursLbl.isHidden = false
+                resultLbl.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
+        
+        
+        
         
     }
+    
+    // Actions
+    @IBAction func clearCalcTapped(_ sender: Any) {
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
+        wageText.text = ""
+        priceText.text = ""
+    }
+    
 
 
 }
